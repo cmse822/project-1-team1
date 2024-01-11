964560eebb8be96b321bd85f31434eed31f6dcb3
Benjamin DalFavero <bdalfavero@gmail.com>
2024-01-11 12:13:17 -0500

    Ah, now the memory allocation is fixed. First you allocate the pointers to the rows, then the rows themselves. When freeing, you free each row, then free the pointers to the rows.

--- matrix.c
+++ matrix.c
@@ -2,8 +2,9 @@
 #include "matrix.h"
 
 void matrix_alloc(matrix_t *mat) {
+	mat->data = malloc(mat->rows * sizeof(float *));
 	for (int i = 0; i < mat->rows; i++) {
-		mat->data[i] = malloc(mat->cols * sizeof(float *));
+		mat->data[i] = malloc(mat->cols * sizeof(float));
 	}
 }
 
@@ -11,4 +12,5 @@ void matrix_free(matrix_t *mat) {
 	for (int i = 0; i < mat->rows; i++) {
 		free(mat->data[i]);
 	}
+	free(mat->data);
 }
