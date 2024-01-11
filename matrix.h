0652c9e8eac935cfc117c1258bec821f86cb9dd4
Benjamin DalFavero <bdalfavero@gmail.com>
2024-01-11 11:34:28 -0500

    Notes on storage convention.

--- matrix.h
+++ matrix.h
@@ -1,7 +1,11 @@
 // Header for matrix data type and functions.
 
+/* Matrix is stored as a pointer to a pointer to a float.
+ * We will store the array such that A[i][j] is the element
+ * at row i and column j.
+ */
 typedef struct matrix_s {
-	size_t rows,
-	size_t cols,
-	float **data
+	size_t rows;
+	size_t cols;
+	float **data;
 } matrix_t;
