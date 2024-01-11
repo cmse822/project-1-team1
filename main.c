09e94eaecc5c68e7095ab4d836bcb0076b7ecb34
Benjamin DalFavero <bdalfavero@gmail.com>
2024-01-11 15:39:28 -0500

    There was an error in the printing code where it iterated over i and j both from zero to the number of rows, instead of the number of rows and columns, respectively. Fixed.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 void print_matrix(matrix_t mat) {
 	for (int i = 0; i < mat.rows; i++) {
-		for(int j = 0; j < mat.rows; j++) {
+		for(int j = 0; j < mat.cols; j++) {
 			printf("%f ", mat.data[i][j]);
 		}
 		printf("\n");
@@ -16,22 +16,28 @@ int main() {
 	matA.rows = 5;
 	matA.cols = 3;
 	matrix_alloc(&matA);
+	print_matrix(matA);
 	fill_constant_matrix(&matA, 1.0);
 	print_matrix(matA);
+	printf("\n");
 
 	matrix_t matB;
 	matB.rows = 3;
-	matB.cols = 7;
+	matB.cols = 2;
 	matrix_alloc(&matB);
-	fill_constant_matrix(&matB, 1.0);
 	print_matrix(matB);
+	fill_constant_matrix(&matB, -2.0);
+	print_matrix(matB);
+	printf("\n");
 
 	matrix_t matC;
 	matC.rows = 5;
-	matC.cols = 7;
+	matC.cols = 2;
 	matrix_alloc(&matC);
+	print_matrix(matC);
 	fill_constant_matrix(&matC, 0.0);
 	print_matrix(matC);
+	printf("\n");
 
 	matrix_multiply(matA, matB, &matC);
 
